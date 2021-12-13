6b270d5820d8ad0e0090fcd999ecf9cd12c3bb50
ginohmk <michotall95@gmail.com>
2021-12-13 17:54:17 +0100

    Fix linter error

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
-const ham = document.querySelector(".hide-ham");
-const menuLink = document.querySelector(".desktop-menu");
-const cancelMenu = document.querySelector(".cancel-dropDown");
-const navLinks = Array.from(document.getElementsByClassName("link"));
-const navH1 = document.querySelector(".greet");
-const navP = document.querySelector(".banner-detail");
-const letConnect = document.querySelector(".connect");
-const logo = document.querySelector(".logo");
-
-ham.addEventListener("click", () => {
-  menuLink.classList.toggle("hide");
-  navH1.classList.toggle("blur");
-  navP.classList.toggle("blur");
-  letConnect.classList.toggle("blur");
-  ham.classList.toggle("blur");
-  logo.classList.toggle("blur");
-});
-
-cancelMenu.addEventListener("click", closeMobileMenu);
-
-for (links in navLinks) {
-  navLinks[links].addEventListener("click", closeMobileMenu);
-}
+const ham = document.querySelector('.hide-ham');
+const menuLink = document.querySelector('.desktop-menu');
+const cancelMenu = document.querySelector('.cancel-dropDown');
+const navLinks = Array.from(document.getElementsByClassName('link'));
+const navH1 = document.querySelector('.greet');
+const navP = document.querySelector('.banner-detail');
+const letConnect = document.querySelector('.connect');
+const logo = document.querySelector('.logo');
 
 function closeMobileMenu() {
-  menuLink.classList.toggle("hide");
-  navH1.classList.toggle("blur");
-  navP.classList.toggle("blur");
-  letConnect.classList.toggle("blur");
-  ham.classList.toggle("blur");
-  logo.classList.toggle("blur");
+  menuLink.classList.toggle('hide');
+  navH1.classList.toggle('blur');
+  navP.classList.toggle('blur');
+  letConnect.classList.toggle('blur');
+  ham.classList.toggle('blur');
+  logo.classList.toggle('blur');
 }
+
+ham.addEventListener('click', closeMobileMenu);
+
+cancelMenu.addEventListener('click', closeMobileMenu);
+
+Object.keys(navLinks).forEach((k) => {
+  if (k !== '') {
+    navLinks[k].addEventListener('click', closeMobileMenu);
+  }
+});
